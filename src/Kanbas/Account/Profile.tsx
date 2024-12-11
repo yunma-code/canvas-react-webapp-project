6cf39819f1223a30e2ec3363e866ad75b2ef007f
Jashuu <iqjs0124@gmail.com>
2024-12-11 13:13:57 -0500

    add update logic

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -11,12 +11,16 @@ export default function Profile() {
   const navigate = useNavigate();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
 
+  const [showModal, setShowModal] = useState(false);
+
   const updateProfile = async () => {
     const updatedProfile = await client.updateUser(profile);
     dispatch(setCurrentUser(updatedProfile));
+    setShowModal(true);
   };
+
   const fetchProfile = () => {
-    if(!currentUser) return navigate("/Kanbas/Account/Signin");
+    if (!currentUser) return navigate("/Kanbas/Account/Signin");
     setProfile(currentUser);
   };
   const signout = async () => {
@@ -24,42 +28,66 @@ export default function Profile() {
     dispatch(setCurrentUser(null));
     navigate("/Kanbas/Account/Signin");
   };
+
   useEffect(() => { fetchProfile(); }, []);
+
   return (
     <div className="wd-profile-screen">
       <h3>Profile</h3>
       {profile && (
         <div>
 
-      <input defaultValue={profile.username} id="wd-username"
-        placeholder="username" className="form-control mb-2"
-        onChange={(e) => setProfile({ ...profile, username: e.target.value })} />
-      <input defaultValue={profile.password} id="wd-password" 
-        placeholder="password" type="password" className="form-control mb-2"
-        onChange={(e) => setProfile({ ...profile, password: e.target.value })} />
-      <input defaultValue={profile.firstName} id="wd-firstname"
-        placeholder="First Name" className="form-control mb-2"
-        onChange={(e) => setProfile({ ...profile, firstName: e.target.value })}/>
-      <input defaultValue={profile.lastName} id="wd-lastname" 
-        placeholder="Last Name" className="form-control mb-2"
-        onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
-      <input defaultValue={profile.dob} id="wd-dob" type="date"
-        className="form-control mb-2"
-        onChange={(e) => setProfile({ ...profile, dob: e.target.value })} />
-      <input defaultValue={profile.email} id="wd-email" 
-        type="email" className="form-control mb-2" 
-        onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
-      <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
-        id="wd-role" className="form-control mb-2">
-        <option value="USER">User</option>
-        <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option>
-        <option value="STUDENT">Student</option>
-      </select>
-      <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
-      <button onClick={signout} className="btn btn-danger w-100 mb-2" id="wd-signout-btn">Sign out</button>
-    </div>
+          <input defaultValue={profile.username} id="wd-username"
+            placeholder="username" className="form-control mb-2"
+            onChange={(e) => setProfile({ ...profile, username: e.target.value })} />
+          <input defaultValue={profile.password} id="wd-password"
+            placeholder="password" type="password" className="form-control mb-2"
+            onChange={(e) => setProfile({ ...profile, password: e.target.value })} />
+          <input defaultValue={profile.firstName} id="wd-firstname"
+            placeholder="First Name" className="form-control mb-2"
+            onChange={(e) => setProfile({ ...profile, firstName: e.target.value })} />
+          <input defaultValue={profile.lastName} id="wd-lastname"
+            placeholder="Last Name" className="form-control mb-2"
+            onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
+
+          <input defaultValue={profile.dob} id="wd-dob" type="date"
+            className="form-control mb-2"
+            onChange={(e) => setProfile({ ...profile, dob: e.target.value })} />
+
+          <input defaultValue={profile.email} id="wd-email"
+            type="email" className="form-control mb-2"
+            onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
+          <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
+            id="wd-role" className="form-control mb-2">
+            <option value="USER">User</option>
+            <option value="ADMIN">Admin</option>
+            <option value="FACULTY">Faculty</option>
+            <option value="STUDENT">Student</option>
+          </select>
+          
+          <div>
+            <button onClick={updateProfile} className="btn btn-primary w-100 mb-2">
+              Update
+            </button>
+
+            {showModal && (
+              <div className="profile-modal-overlay">
+                <div className="profile-modal-content">
+                  <h4>Profile Updated Successfully</h4>
+                  <button onClick={() => setShowModal(false)} className="btn btn-secondary">
+                    Close
+                  </button>
+                </div>
+              </div>
+            )}
+
+          </div>
+
+
+          <button onClick={signout} className="btn btn-danger w-100 mb-2" id="wd-signout-btn">Sign out</button>
+        </div>
       )}
     </div>
-);}
+  );
+}
 
